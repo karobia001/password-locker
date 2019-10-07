2dae2095a235cba7b7d8b223ac9378a61d6ab561
karobia001 <karobiamaina81@gmail.com>
2019-10-07 08:08:58 +0300

    create an empty loop and create a constractor that we pass five arguments to

--- user.py
+++ user.py
@@ -4,3 +4,13 @@ class Users:
     """
 
     pass
+
+account_details = [] #This is where we will store all the accounts and their details
+
+    def __init__(self,account_name,username,email,password): #the intance variables that will represent the users details for different accounts
+
+         
+            self.account_name = account_name
+            self.username = username
+            self.email = email
+            self.password = password
