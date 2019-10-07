7f0d49c3459dc20242cf5c95e30acc68e52bc757
karobia001 <karobiamaina81@gmail.com>
2019-10-07 11:29:21 +0300

    create a save user details information/usr/bin/python3

--- user.py
+++ user.py
@@ -1,16 +1,19 @@
-class Users:
-    """
-    Class that generates new instances of users
-    """
 
-    pass
 
-account_details = [] #This is where we will store all the accounts and their details
+class User:
+    
+    
+    
+    user_details = []
 
-def __init__(self,account_name,username,email,password): #the intance variables that will represent the users details for different accounts
-
-         
-            self.account_name = account_name
-            self.username = username
-            self.email = email
-            self.password = password
+    def __init__(self, account, first_name, last_name, phone_number, email_address, username, password):
+        self.account = account
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email_address = email_address
+        self.username = username
+        self.password = password
+    
+    def save_user(self):
+        User.user_details.append(self)
