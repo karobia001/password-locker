14e01993226bbceb9b7786db4d463f6e788b8375
karobia001 <karobiamaina81@gmail.com>
2019-10-07 14:45:17 +0300

    install pyper clip inorder to copy and paste text to the clip board

--- user_test.py
+++ user_test.py
@@ -1,4 +1,4 @@
-
+import pyperclip
 import unittest
 from user import User
 
@@ -15,30 +15,41 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.email_address, "karobiamaina81@gmail.com")
         self.assertEqual(self.new_user.username, "maich")
         self.assertEqual(self.new_user.password, "maich001")
+    
+    
         
     def test_save_user(self):
         self.new_user.save_user()
         self.assertEqual(len(User.user_details), 1)
     
-    
+    def tearDown(self):
+        User.user_details = []  
+      
     def test_save_multiple_user(self):
         self.new_user.save_user()
         test_user = User("facebook","karobia", "maina", "0724550408","karobiamaina81@gamil.com", "maich", "maich001")
         test_user.save_user()
         self.assertEqual(len(User.user_details), 2)
+     
+
     
-    def tearDown(self):
-        User.user_details = []
+    def test_user_exists(self):
+        self.new_user.save_user()
+        test_user = User("facebook","karobia", "maina", "0724550408","karobiamaina81@gamil.com", "maich", "maich001")
+        test_user.save_user()
+
+        user_exists = User.user_exists("0724550408")
+        self.assertTrue(user_exists)
         
     def delete_user(self):
 
         '''
         delete_user method deletes a saved user from the user_details
         '''
-
         User.user_details.remove(self)
-
-
+        
+        self.assertEqual(self.new_user.email_address,pyperclip.paste())
+    
 if __name__ == '__main__':
     unittest.main()
 
